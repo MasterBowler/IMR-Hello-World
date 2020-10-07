1973b352371c4a1f656d4d4db2327256c15c3c63
Mihai Manolache <manolachemihai09@gmail.com>
2020-10-07 15:54:09 +0300

    asset
    I hope these are the things I need to push

---
+++ Assets/AnimateUnitychan.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateUnitychan : MonoBehaviour
+{
+    public Animator animator;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetBool("isCameraClose", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        animator.SetBool("isCameraClose", false);
+    }
+}
